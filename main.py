d27507e3b35af71c3312b1c9c867653e72958494
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-29 00:33:05 +0200

    Step 32

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def verify_card_number(card_number):
         sum_of_even_digits += number
     total =sum_of_odd_digits + sum_of_even_digits
     print(total)
-
+    return 0 == total % 10
 
 
 def main():
