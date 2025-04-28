f120d5b5013cbb4b42505484305b87d916e80801
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-29 00:35:19 +0200

    Step 34

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def verify_card_number(card_number):
 
 
 def main():
-    card_number = '4111-1111-4555-1142'
+    card_number = '5111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
     if verify_card_number(translated_card_number):
