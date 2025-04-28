1022e6d4241effbab798a9914735fd12c854b1e7
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-29 00:19:49 +0200

    Step 21

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ def verify_card_number(card_number):
     card_number_reversed = card_number[::-1]
     odd_digits = card_number_reversed[::2]
     for digit in odd_digits:
-        print(digit)
-        sum_of_odd_digits += digit
+        sum_of_odd_digits += int(digit)
+    print(sum_of_odd_digits)
 
 def main():
     card_number = '4111-1111-4555-1142'
