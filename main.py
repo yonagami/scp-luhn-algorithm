0fdf97f6247967d9dd88949bc141ab754bde4778
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-29 00:14:04 +0200

    Step 17

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
     card_number_reversed = card_number[::-1]
-    #print(card_number_reversed)
+    odd_digits = card_number_reversed[::2]
 
 
 def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
-    #print(translated_card_number)
     verify_card_number(translated_card_number)
 
 
