7e76d1734c94fea909c4602b1aabf7083a0a2601
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-29 00:23:48 +0200

    Step 25

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def verify_card_number(card_number):
     odd_digits = card_number_reversed[::2]
     for digit in odd_digits:
         sum_of_odd_digits += int(digit)
-    print(sum_of_odd_digits)
+    
     sum_of_even_digits = 0
     even_digits = card_number_reversed[1::2]
     for digit in even_digits:
