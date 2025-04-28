bbd644a37206833f6fe97b49324f16609ea48ed7
Yonagami <yonagami@yonagami.blahblahblah>
2025-04-29 00:00:11 +0200

    Step 5

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
+    print(translated_card_number)
+
+
+
+
+
 
